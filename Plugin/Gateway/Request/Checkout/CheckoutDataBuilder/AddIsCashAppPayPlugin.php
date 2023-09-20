596eaab989431e92d09fadd8be93ee7a4b81fa46
Ghata Mehta <53248085+afterpayplugins@users.noreply.github.com>
2023-09-20 11:30:58 +1000

    Release version 5.0.4

--- Plugin/Gateway/Request/Checkout/CheckoutDataBuilder/AddIsCashAppPayPlugin.php
+++ Plugin/Gateway/Request/Checkout/CheckoutDataBuilder/AddIsCashAppPayPlugin.php
@@ -3,13 +3,17 @@
 namespace Afterpay\CashApp\Plugin\Gateway\Request\Checkout\CheckoutDataBuilder;
 
 use Afterpay\Afterpay\Gateway\Request\Checkout\CheckoutDataBuilder;
+use Afterpay\CashApp\Gateway\Config\Config;
 
 class AddIsCashAppPayPlugin
 {
     public function afterBuild(CheckoutDataBuilder $subject, array $result, $buildSubject)
     {
         if (!empty($buildSubject['quote'])
-            && ($buildSubject['quote'])->getPayment()->getMethod() === \Afterpay\CashApp\Gateway\Config\Config::CODE) {
+            && $buildSubject['quote']->getPayment()->getMethod() === Config::CODE
+            && isset($result['merchant']['redirectConfirmUrl'])
+            && strpos($result['merchant']['redirectConfirmUrl'], Config::CODE) !== false
+        ) {
             $result['isCashAppPay'] = true;
         }
 
