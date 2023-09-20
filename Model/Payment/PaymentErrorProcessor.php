6494f3c38ddf30540c61129adc39c79dc83d9482
Ghata Mehta <53248085+afterpayplugins@users.noreply.github.com>
2023-09-20 11:32:20 +1000

    Release version 4.0.4

---
+++ Model/Payment/PaymentErrorProcessor.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Afterpay\CashApp\Model\Payment;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Quote\Model\Quote;
+use Magento\Quote\Model\Quote\Payment;
+
+class PaymentErrorProcessor extends \Afterpay\Afterpay\Model\Payment\PaymentErrorProcessor
+{
+    /**
+     * Rewrites the payment name in Localized exceptions.
+     *
+     * @param Quote      $quote
+     * @param \Throwable $e
+     * @param Payment    $payment
+     *
+     * @return int
+     * @throws LocalizedException
+     */
+    public function execute(Quote $quote, \Throwable $e, Payment $payment): int
+    {
+        try {
+            return parent::execute($quote, $e, $payment);
+        } catch (LocalizedException $e) {
+            throw new LocalizedException(__(str_replace('Afterpay', 'Cash App', $e->getMessage())));
+        }
+    }
+}
